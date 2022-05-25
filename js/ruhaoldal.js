d44995580aa0770011b6b30bbd637d19d32bff56
rohacsidana <51097937+rohacsidana@users.noreply.github.com>
2022-05-25 17:58:25 +0200

    camelcase eliras

--- js/ruhaoldal.js
+++ js/ruhaoldal.js
@@ -20,7 +20,7 @@ function ruhaoldalminden() {
 
 
   for (let index = 0; index < aktualis.kepek.length; index++) {
-    txt += `<img class='mySlides' src=ruhakepek/${mappa}/${aktualis.kepek[index]}.jpg style="width:100%">`
+    txt += `<img class='mySlides' src=ruhaKepek/${mappa}/${aktualis.kepek[index]}.jpg style="width:100%">`
   }
 
   txt += `<div class="w3-center w3-container w3-section w3-large w3-text-black w3-display-bottommiddle" style="width:100%">
